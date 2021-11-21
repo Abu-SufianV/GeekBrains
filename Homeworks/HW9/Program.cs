16ad2582866775686ac6fdd1e19f3bff7eae0ab6
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-21 21:21:56 +0300

    HW9 - Complete

---
+++ Homeworks/HW9/Program.cs
@@ -0,0 +1,8 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+Console.Write("\nВведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Последняя цифра числа {num} - {num % 10}");
+
+Console.WriteLine("\n");
