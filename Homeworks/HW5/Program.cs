9490fe6ef5d46b5ae677b2ee1e993769296c7b00
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-21 15:21:02 +0300

    HW5 - Complete

---
+++ Homeworks/HW5/Program.cs
@@ -0,0 +1,13 @@
+﻿//  5. Написать программу вычисления значения функции y = f(a)
+
+using System;
+
+double function(double num)
+{
+    return Math.Sin(Math.Pow(num, 10));
+}
+
+Console.WriteLine("\nФункция: f(x) = sin(x ^ 10)");
+Console.Write("\nВведите число: ");
+
+Console.Write($"\nОтвет: {function(Convert.ToDouble(Console.ReadLine()))}");
