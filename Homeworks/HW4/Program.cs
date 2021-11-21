5b1ad65f0c0088e3462090f4862cb9df96c6d4cf
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-21 15:09:36 +0300

    HW4 - Complete

---
+++ Homeworks/HW4/Program.cs
@@ -0,0 +1,24 @@
+﻿// 4. Найти максимальное из трех чисел
+
+using System;
+
+Console.Write("Введите 1-е число: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int num_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num_1;
+
+if (num_2 > max)
+{
+    max = num_2;
+}
+if (num_3 > max)
+{
+    max = num_3;
+}
+
+Console.WriteLine($"Максимальное число - {max}");
+
