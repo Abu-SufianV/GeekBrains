4a16d56fcabbd863b0a94e99e42568b212db7d2e
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-21 15:41:28 +0300

    HW7 - Complete

---
+++ Homeworks/HW7/Program.cs
@@ -0,0 +1,22 @@
+﻿//  7. Показать числа от -N до N
+
+void show_numbers(int first, int second)
+{
+    Console.WriteLine("Список чисел: ");
+    for (int i = first; i <= second; i++)
+    {
+        Console.Write($"{i}  ");
+    }
+}
+
+Console.Write("\nВведите 1-e число: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nВведите 2-e число: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+show_numbers(num_1, num_2);
+
+Console.WriteLine("\n");
