af91be1c9282ab992fb894a50e269644a33facbf
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-21 15:33:50 +0300

    HW6 - Complete

---
+++ Homeworks/HW6/Program.cs
@@ -0,0 +1,17 @@
+﻿// 6. Выяснить является ли число чётным
+
+using System;
+
+Console.Write("\nВведите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Число {num} - чётное");
+}
+else
+{
+    Console.WriteLine($"Число {num} - не чётное");
+}
+
+Console.WriteLine("\n");
