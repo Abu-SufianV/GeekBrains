b1649084fc18a98574919b46f82f4e1f8094502b
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-21 21:17:00 +0300

    HW8 - Complete

---
+++ Homeworks/HW8/Program.cs
@@ -0,0 +1,20 @@
+﻿// 8. Показать четные числа от 1 до N
+
+void show_even_numbers(int number)
+{
+    Console.WriteLine($"Список чётных чисел от 1 до {number}: ");
+    for (int i = 1; i <= number; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i}  ");
+        }
+
+    }
+}
+
+Console.Write("\nВведите число: ");
+
+show_even_numbers(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine("\n");
