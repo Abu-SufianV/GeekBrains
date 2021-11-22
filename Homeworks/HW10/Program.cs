63af27d600078d50006bfe2b98a45a7e620f10b2
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-22 11:40:34 +0300

    HW10 - Complete

---
+++ Homeworks/HW10/Program.cs
@@ -0,0 +1,8 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+Console.Write("\nВведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nВторая цифра числа {num} - {num / 10 % 10}");
+
+Console.WriteLine("\n");
