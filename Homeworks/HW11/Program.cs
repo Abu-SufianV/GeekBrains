92079f78b9c033e03f29eb824e82ca8dae1c9bcb
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-22 12:10:27 +0300

    HW11 - Complete

---
+++ Homeworks/HW11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Write("\nВведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int x = num / 10;
+int y = num % 10;
+
+Console.WriteLine();
+
+if (x > y)
+{
+    Console.Write($"В числе {num} наибольшая цифра - {x}");
+}
+else
+{
+    Console.Write($"В числе {num} наибольшая цифра - {y}");
+}
+
+Console.WriteLine("\n");
