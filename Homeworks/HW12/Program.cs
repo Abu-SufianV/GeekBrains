d002d58a88aff85ed8c070a3342d747ba46e550c
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-23 21:36:21 +0300

    HW12 - Complete

---
+++ Homeworks/HW12/Program.cs
@@ -0,0 +1,12 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+int num_delete(int number)
+{
+    int num_1 = number / 100 * 10;
+    int num_2 = number % 10;
+
+    return num_1 + num_2;
+}
+
+Console.WriteLine("Введите число: ");
+Console.WriteLine(num_delete(Convert.ToInt32(Console.ReadLine())));
