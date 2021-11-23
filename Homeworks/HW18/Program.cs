9635b952e599cb619fa6b72bcf7726a458d9ea07
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-24 01:49:16 +0300

    HW18 - Complete

---
+++ Homeworks/HW18/Program.cs
@@ -0,0 +1,14 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+Console.WriteLine("\nНеобходимо проверить истинность утверждения: ¬(X ⋁ Y) = ¬X ⋀ ¬Y\n\nВведите значения X и Y (значения не равные нуля, равняются ЛОЖЬ)\n");
+
+Console.Write("Введите X: ");
+bool X = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+
+Console.Write("Введите Y: ");
+bool Y = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+
+if (!(X & Y) == !X | !Y)
+{
+    Console.Write("Утверждение инстинно");
+}
