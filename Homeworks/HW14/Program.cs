7ba1df8938b81f50369ebb5c7f502f5ee3a8410f
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-24 01:14:17 +0300

    HW14 - Complete

---
+++ Homeworks/HW14/Program.cs
@@ -0,0 +1,17 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+string search_num(string number)
+{
+    if (number.Length >= 3)
+    {
+        return $"Третья цифра числа {number[2]}";
+    }
+
+    return "Данное число меньше 100";
+}
+
+Console.Write("Введите число: ");
+
+Console.WriteLine(search_num(Console.ReadLine()));
+
+Console.WriteLine("\n");
