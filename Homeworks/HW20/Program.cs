476fa162b18a305625dce8051b6ab477e835cf9c
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-24 02:21:18 +0300

    HW20 - Complete

---
+++ Homeworks/HW20/Program.cs
@@ -0,0 +1,24 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+
+string dots(int quarter)
+{
+    switch (quarter)
+    {
+        case 1:
+            return $"Диапозон {quarter} четверти:\nX: (0 , +]\nY: (0 , +]";
+
+        case 2:
+            return $"Диапозон {quarter} четверти:\nX: [- , 0)\nY: (0 , +]";
+
+        case 3:
+            return $"Диапозон {quarter} четверти:\nX: [- , 0)\nY: [- , 0)";
+
+        case 4:
+            return $"Диапозон {quarter} четверти:\nX - (0 , +]\nY: [- , 0)";
+
+        default: return "Вы введи неверное значение";
+    }
+}
+
+Console.Write("Введите четверть: ");
+Console.Write(dots(Convert.ToInt32(Console.ReadLine())));
