ab4ee023196354398ebab4b69b783117f6da32a9
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-24 01:21:33 +0300

    HW15 - Complete

---
+++ Homeworks/HW15/Program.cs
@@ -0,0 +1,17 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num % 7 == 0) && (num % 23 == 0))
+{
+    Console.WriteLine($"Число {num} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно 7 и 23");
+}
+
+
+Console.WriteLine("\n");
+
