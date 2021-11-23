fdb13f4e8d8fd95d3f75aececeef15251fd3e9e3
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-24 01:37:16 +0300

    HW17 - Complete

---
+++ Homeworks/HW17/Program.cs
@@ -0,0 +1,18 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите 1-е число: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-е число: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+if ((num_1 * num_1 == num_2) || (num_2 * num_2 == num_1))
+{
+    Console.Write("Одно из чисел является квадратом другого");
+}
+else
+{
+    Console.Write("Ни одно из чисел не является квадратом другого");
+}
+
+Console.WriteLine("\n");
