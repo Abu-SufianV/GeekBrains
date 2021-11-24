5ca427bd23e614e0cb1af0d4676e8c69ad488d0b
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-25 00:27:43 +0300

    HW23 - Complete

---
+++ Homeworks/HW23/Program.cs
@@ -0,0 +1,29 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+
+void get_table(int size)
+{
+    if (size <= 10)
+    {
+        for (int num = 1; num <= size; num++)
+        {
+            Console.Write($"{num * num} ");
+        }
+    }
+    else
+    {
+        for (int num = 1; num <= size; num++)
+        {
+            Console.Write($"{num * num} ");
+            if (num % 10 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
+
+Console.Write("Введите размер таблицы: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+get_table(number);
