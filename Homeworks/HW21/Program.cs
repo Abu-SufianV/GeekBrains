7fafc22092a0dd43017c371eb2097293b440f4ff
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-24 22:09:48 +0300

    HW21 - Complete

---
+++ Homeworks/HW21/Program.cs
@@ -0,0 +1,19 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом
+
+string palindrome(string number)
+{
+    string past_1 = $"{number[0]}{number[1]}";
+    string past_2 = $"{number[4]}{number[3]}";
+
+    if (past_1 == past_2)
+    {
+        return "Число является палиндромом";
+    }
+    return "Число НЕ является палиндромом";
+
+}
+
+Console.Write("\nВведите число: ");
+Console.WriteLine(palindrome(Console.ReadLine()));
+
+Console.WriteLine();
