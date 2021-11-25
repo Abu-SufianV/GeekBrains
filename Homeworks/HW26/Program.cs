ec477a6da0f27e9474b70269799067373f22c610
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-25 15:20:43 +0300

    HW26 - Complete

---
+++ Homeworks/HW26/Program.cs
@@ -0,0 +1,19 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+double exponential(int number, int degree)
+{
+    double result = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степени: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Число {num} в степени {degree} = {exponential(num, degree)} ");
