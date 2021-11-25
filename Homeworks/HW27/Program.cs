f52ac97e1ada566bb888530da2c075468267ee38
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-25 15:20:55 +0300

    HW27 - Complete

---
+++ Homeworks/HW27/Program.cs
@@ -0,0 +1,11 @@
+﻿// 27. Определить количество цифр в числе
+
+int digit_count(int number)
+{
+    return number.ToString().Length;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Количество цифр в числе {num} = {digit_count(num)}");
