5ab7a6b4ca6eeb08eb036831c1105dd303a30c1c
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-11-25 14:37:54 +0300

    HW25 - Complete

---
+++ Homeworks/HW25/Program.cs
@@ -0,0 +1,19 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+int sum_array(int size)
+{
+    int[] array = new int[size];
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i + 1;
+        sum = sum + array[i];
+    }
+
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Сумма числел от 1 до {num} = {sum_array(num)}");
