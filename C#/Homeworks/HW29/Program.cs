7be139e43402192a4ec365729785bba3621b6e03
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-02 00:17:30 +0300

    HW29 - Complete

---
+++ C#/Homeworks/HW29/Program.cs
@@ -0,0 +1,19 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+int sum_array(int size)
+{
+    int[] array = new int[size];
+    int prod = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i + 1;
+        prod *= array[i];
+    }
+
+    return prod;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Произведение числел от 1 до {num} = {sum_array(num)}");
