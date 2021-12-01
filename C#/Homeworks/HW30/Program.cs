d7b5c1d9bb2eefb2184239c77865d8329158f700
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-02 00:49:25 +0300

    HW30 - Complete

---
+++ C#/Homeworks/HW30/Program.cs
@@ -0,0 +1,18 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+void last_digit_even(int amount)
+{
+    for (int i = 1; i <= amount; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(String.Format("{0,7}", Math.Pow(i, 3)));
+        }
+    }
+}
+
+Console.Write("\n\n\nВведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\n\nПоказать кубы чисел, заканчивающихся на четную цифру от 1 до {num}:");
+last_digit_even(num);
