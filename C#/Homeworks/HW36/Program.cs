38a34a8c6599b62c1df86d8de35be2477388be6a
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-03 00:10:45 +0300

    HW36 - Complete

---
+++ C#/Homeworks/HW36/Program.cs
@@ -0,0 +1,31 @@
+﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] array = new int[30];
+int even_numbers = 0;
+
+void get_array(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(String.Format("{0,5}", array[i]));
+        if ((i + 1) % 10 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0)
+    {
+        even_numbers++;
+    }
+}
+
+get_array(array);
+
+Console.WriteLine($"В данном массиве {even_numbers} чётных чисел и {Math.Abs(even_numbers - array.Length)} нечётных\n");
