a2a75796bd76e167d419d2a73cf3ce323d40f877
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-02 14:46:41 +0300

    HW31 - Complete

---
+++ C#/Homeworks/HW31/Program.cs
@@ -0,0 +1,16 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+int[] Arr = new int[8];
+
+void get_array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(String.Format("{0,5}", array[i]));
+        if ((i + 1) % 10 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
+get_array(Arr);
