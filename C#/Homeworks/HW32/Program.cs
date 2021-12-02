57837e5b2847e76c5cc0a940a355560941c0cedd
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-02 15:21:08 +0300

    HW32 - Complete

---
+++ C#/Homeworks/HW32/Program.cs
@@ -0,0 +1,31 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+Random rand = new Random();
+
+int[] Arr = new int[8];
+
+void get_array(int[] array)
+{
+    Console.WriteLine("Массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(String.Format("{0,5}", array[i]));
+        if ((i + 1) % 10 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
+for (int i = 0; i < Arr.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Arr[i] = 1;
+    }
+    else
+    {
+        Arr[i] = 0;
+    }
+}
+get_array(Arr);
