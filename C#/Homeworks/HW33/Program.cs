91c548c01b0081771f3539f986e9444fbd51021f
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-02 22:52:10 +0300

    HW33 - Complete
    HW33 - Added minor changes

---
+++ C#/Homeworks/HW33/Program.cs
@@ -0,0 +1,24 @@
+﻿// 33. Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+
+
+int[] Arr = new int[12];
+
+void get_array(int[] array)
+{
+    Console.WriteLine("Массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(String.Format("{0,5}", array[i]));
+        if ((i + 1) % 10 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
+
+for (int i = 0; i < Arr.Length; i++)
+{
+    Arr[i] = new Random().Next(0, 10);
+}
+
+get_array(Arr);
