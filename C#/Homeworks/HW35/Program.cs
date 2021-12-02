70b991993fa9d1c9d711dd25cbbecde9e4d6b355
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-02 23:50:00 +0300

    HW35 - Complete

---
+++ C#/Homeworks/HW35/Program.cs
@@ -0,0 +1,36 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число 
+
+int[] array = new int[10];
+
+void get_array(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(String.Format("{0,5}", array[i]));
+        if ((i + 1) % 10 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+get_array(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (num == array[i])
+    {
+        Console.Write($"Число {num} есть в данном массиве\nНаходится на {i + 1} месте\n\n");
+        break;
+    }
+}
