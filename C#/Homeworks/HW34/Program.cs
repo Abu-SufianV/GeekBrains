bb446fd936edb0cb0b29a85f50b14f07debdca94
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-02 22:59:49 +0300

    HW34 - Complete

---
+++ C#/Homeworks/HW34/Program.cs
@@ -0,0 +1,31 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+
+int[] array = new int[10];
+
+void get_array(int[] array)
+{
+    Console.WriteLine("\nМассив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(String.Format("{0,5}", array[i]));
+        if ((i + 1) % 10 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+}
+
+get_array(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+}
+
+get_array(array);
