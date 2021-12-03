716a9040af06208b565cf1e9d2b0b59c74d3e6d4
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-04 01:40:45 +0300

    HW41 - Complete

---
+++ C#/Homeworks/HW41/Program.cs
@@ -0,0 +1,24 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника 
+
+void is_triangle(int A, int B, int C)
+{
+    if ((A < B + C) && (B < A + C) && (C < A + B))
+    {
+        Console.WriteLine("\nДанные числа являются сторонами треугольника\n");
+    }
+    else
+    {
+        Console.WriteLine("\nДанные числа НЕ являются сторонами треугольника\n");
+    }
+}
+
+Console.Write("\nВведите сторону А: ");
+int side_A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите сторону B: ");
+int side_B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите сторону C: ");
+int side_C = Convert.ToInt32(Console.ReadLine());
+
+is_triangle(side_A, side_B, side_C);
