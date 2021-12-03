7ee9d805ecf5fef52577c406d683e6724e29775f
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-04 01:50:27 +0300

    HW42 - Complete

---
+++ C#/Homeworks/HW42/Program.cs
@@ -0,0 +1,16 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("\nУкажите кол-во вводимых чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+int null_numbers = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{i + 1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    null_numbers += (array[i] > 0) ? 1 : 0;
+}
+
+Console.WriteLine($"\nКол-во чисел большие нуля: {null_numbers}");
