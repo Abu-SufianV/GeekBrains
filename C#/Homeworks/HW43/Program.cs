862a8537093bfd2a6f1a9bf5f2b760ecaa3e80c3
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-07 01:26:28 +0300

    HW43 - Complete

---
+++ C#/Homeworks/HW43/Program.cs
@@ -0,0 +1,11 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное число 
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string decimal_to_binary(int num)
+{
+    return Convert.ToString(num, 2);
+}
+
+Console.WriteLine($"\nЧисло {number} в двоичной системе счисления равен: {decimal_to_binary(number)}\n");
