1b1fd260c6f19bd2b20f79e8af5b5bed8ce39488
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-07 18:43:33 +0300

    HW46 - Complete

---
+++ C#/Homeworks/HW46/Program.cs
@@ -0,0 +1,17 @@
+﻿// 46. Написать программу масштабирования фигуры
+
+Console.Write($"\nВведите точки фигуры: ");
+string digitsRow = Console.ReadLine();
+Console.Write($"\nМножитель масштабирования: ");
+int multiply = Convert.ToInt32(Console.ReadLine());
+// digitsRow.Replace('(');
+
+string[] digits = digitsRow.Split(new char[] { ' ', '(', ')', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+Console.Write($"Точки фигуры при масштабе {multiply}: ");
+for (int i = 0; i < digits.Length; i = i + 2)
+{
+    int num_1 = Convert.ToInt32(digits[i]) * multiply;
+    int num_2 = Convert.ToInt32(digits[i + 1]) * multiply;
+    Console.Write($"({num_1},{num_2}) ");
+}
