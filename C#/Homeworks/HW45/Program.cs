2f74cf22c343f9b880f1221d77ee824bb7f8de5c
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-07 14:03:15 +0300

    HW45 - Complete

---
+++ C#/Homeworks/HW45/Program.cs
@@ -0,0 +1,17 @@
+﻿// 45. Показать числа Фибоначчи
+
+void Fibonachi_nums(int amount)
+{
+    int[] numbers = new int[amount];
+    numbers[0] = 0;
+    numbers[1] = 1;
+    for (int i = 2; i < amount; i++)
+    {
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+    }
+    Console.Write($"\nПоследовательность чисел Фибоначи до {amount} числа: ");
+    Console.WriteLine(String.Join(" ", numbers));
+}
+
+Console.Write($"\nВведите кол-во чисел: ");
+Fibonachi_nums(Convert.ToInt32(Console.ReadLine()));
