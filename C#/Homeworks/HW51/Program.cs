9de6c1abf7f4517e1e729e114c6a7a5a45f089d4
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-15 14:49:31 +0300

    HW 51 - Complete

---
+++ C#/Homeworks/HW51/Program.cs
@@ -0,0 +1,36 @@
+﻿// 51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+
+int m = 10;
+int n = 10;
+
+int[,] matrix = new int[m, n];
+
+void show_matrix(int[,] array)
+{
+    Console.WriteLine($"\nДвумерный массив {array.GetLength(0)} x {array.GetLength(1)}: \n");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("\t {0:0}", (array[i, j]));
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void fill_matrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+}
+
+
+fill_matrix(matrix);
+show_matrix(matrix);
