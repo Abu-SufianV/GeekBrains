ddc7524ea94579b26b4c6c7681501bf69d46af55
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-15 14:42:18 +0300

    HW 50 - Complete

---
+++ C#/Homeworks/HW50/Program.cs
@@ -0,0 +1,52 @@
+﻿// 50. В двумерном массиве n×k заменить четные элементы на противоположные
+
+int m = 10;
+int n = 10;
+
+int[,] matrix = new int[m, n];
+
+void show_matrix(int[,] array)
+{
+    Console.WriteLine($"\nДвумерный массив {array.GetLength(0)} x {array.GetLength(1)}: \n");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("\t {0:0}", (array[i, j]));
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void fill_matrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+        }
+    }
+}
+
+int[,] change_matrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * -1;
+            }
+        }
+    }
+
+    return array;
+}
+
+fill_matrix(matrix);
+show_matrix(matrix);
+change_matrix(matrix);
+show_matrix(matrix);
