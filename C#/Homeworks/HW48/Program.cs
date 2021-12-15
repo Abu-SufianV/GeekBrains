cdee3e69887d40b0fa520f42be470f2a2a664649
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-15 13:53:37 +0300

    HW 48 - Complete

---
+++ C#/Homeworks/HW48/Program.cs
@@ -0,0 +1,21 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+int m = 10;
+int n = 10;
+
+int[,] matrix = new int[m, n];
+
+void show_matrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+            Console.Write($"\t{array[i, j]}", 10);
+        }
+        Console.WriteLine();
+    }
+}
+
+show_matrix(matrix);
