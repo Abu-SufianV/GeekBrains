dd4157d35b51ea1a7a7404a1e03eba7d026a6e9a
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-15 14:22:16 +0300

    HW 49 - Complete

---
+++ C#/Homeworks/HW49/Program.cs
@@ -0,0 +1,21 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+
+int m = 10;
+int n = 10;
+
+double[,] matrix = new double[m, n];
+
+void show_matrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+            Console.Write("\t {0:0.##}", (array[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
+
+show_matrix(matrix);
