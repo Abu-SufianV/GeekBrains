b8e00f5f21e46bf5615aaa66862d707292ef8120
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-15 14:52:31 +0300

    HW 52 - Complete

---
+++ C#/Homeworks/HW52/Program.cs
@@ -0,0 +1,41 @@
+﻿// 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+int m = 10;
+int n = 10;
+
+int[,] matrix = new int[m, n];
+
+void show_matrix(int[,] array)
+{
+    Console.WriteLine($"\nДвумерный массив {array.GetLength(0)} x {array.GetLength(1)}: \n");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("\t {0:0}", (array[i, j]));
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void fill_matrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+
+            if ((i % 2 == 0) && (j % 2 == 0))
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+
+        }
+    }
+}
+
+
+fill_matrix(matrix);
+show_matrix(matrix);
