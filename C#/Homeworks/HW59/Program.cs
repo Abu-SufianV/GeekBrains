9aad30e6f458f546bc7956b571306b4073d6c710
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-21 19:42:32 +0300

    HW 59 - Complete

---
+++ C#/Homeworks/HW59/Program.cs
@@ -0,0 +1,52 @@
+﻿// 59. В прямоугольной матрице найти строку с наименьшей суммой элементов
+
+
+int m = 10;
+int n = 10;
+
+int[,] matrix = new int[m, n];
+
+void show_matrix(int[,] array)
+{
+    Console.WriteLine($"\nДвумерный массив {array.GetLength(0)} x {array.GetLength(1)}: \n");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("\t {0:0}", (array[i, j]));
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void fill_matrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void min_sum_column(int[,] array)
+{
+    int[] rows = new int[array.GetLength(0)];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        rows[i] = sum;
+    }
+    Array.Sort(rows);
+    Console.WriteLine($"Минимальная сумма строк ровна: {rows[0]}");
+}
+fill_matrix(matrix);
+show_matrix(matrix);
+min_sum_column(matrix);
