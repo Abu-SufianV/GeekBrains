11a5286716ed4887c2cf21ba52fce7d2ed6c922a
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2021-12-29 12:21:14 +0300

    fill_array() - added new function
    Создана функция заполнения массив целыми числами

---
+++ final_work/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[10];
+
+void fill_array(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(10);
+    }
+}
+
+// int even_array()
+// {
+
+// }
