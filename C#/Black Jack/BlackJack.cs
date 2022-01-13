6c3376008b90462838fde20675caa304d1e4c0e4
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2022-01-13 19:05:38 +0300

    Create new class "Player"

--- C#/Black Jack/BlackJack.cs
+++ C#/Black Jack/BlackJack.cs
@@ -1,5 +1,45 @@
 ﻿// Игра "Black Jack"
 
+class Player
+{
+    private string name;
+    private int money;
+
+    public Player(string name, int money)
+    {
+        this.name = name;
+        this.money = money;
+    }
+
+    public string GetName()
+    {
+        return this.name;
+    }
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+
+    public int GetMoney()
+    {
+        return this.money;
+    }
+
+    public void SetMoney(int money, bool win)
+    {
+        if (win)
+        {
+            this.money += money;
+        }
+        else
+        {
+            this.money -= money;
+        }
+    }
+
+}
+
 class BlackJack
 {
     private int players_amount = 0;
