9a4725d24565c63bb6e6a9a8415f2cc60a0739f8
Abu-Sufian Vagabov <vagabov.abusufian@mail.ru>
2022-03-01 16:38:27 +0300

    Added new feature
    fill_array()
    fill_matrix()
    show_matrix()

--- Python/Tasks/lib/array.py
+++ Python/Tasks/lib/array.py
@@ -1,3 +1,6 @@
+from random import randint as rand
+
+
 def show_array(arr: list[int]) -> None:
     """
     Выводит в консоль отформатированный массив из целых чисел
@@ -10,3 +13,28 @@ def show_array(arr: list[int]) -> None:
         print("{:6}".format(arr[i]), end="")
     print("\n")
     return None
+
+
+def show_matrix(arr: list) -> None:
+    """
+    Выводит в консоль отформатированный двумерный массив из целых чисел
+
+    :param arr: Массив, который нужно вывести
+    """
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print("{:6}".format(arr[i][j]), end="")
+        print()
+    print("\n")
+    return
+
+
+def fill_array(arr: list[int], value: int):
+    for i in range(len(arr)):
+        arr[i] = rand(0, value)
+
+
+def fill_matrix(arr: list, value: int):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            arr[i][j] = rand(0, value)
